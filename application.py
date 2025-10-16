eac6d3b0931a1aee636a9e775bcc77518c547f61
Chhaya Varshney <chhaya3033@gmail.com>
2025-10-16 12:03:01 +0530

    Changes in application.py

--- application.py
+++ application.py
@@ -15,22 +15,30 @@ def index():
 def predict_datapoint():
     if request.method == 'GET':
         return render_template('home.html')
+    
     else:
-        data = CustomData(
-            gender = request.form.get('gender'),
-            race_ethnicity = request.form.get('race_ethnicity'),
-            parental_level_of_education = request.form.get('parental_level_of_education'),
-            lunch = request.form.get('lunch'),
-            test_preparation_course = request.form.get('test_preparation_course'),
-            reading_score = request.form.get('reading_score'),
-            writing_score = request.form.get('writing_score')
-        )
+        try:
+            data = CustomData(
+                gender = request.form.get('gender'),
+                race_ethnicity = request.form.get('race_ethnicity'),
+                parental_level_of_education = request.form.get('parental_level_of_education'),
+                lunch = request.form.get('lunch'),
+                test_preparation_course = request.form.get('test_preparation_course'),
+                reading_score = request.form.get('reading_score'),
+                writing_score = request.form.get('writing_score')
+            )
 
-        pred_df = data.get_data_as_data_frame()
-        print(pred_df)
-        predict_pipeline = PredictPipeline()
-        results = predict_pipeline.predict(pred_df)
-        return render_template('home.html', result = results)
+            pred_df = data.get_data_as_data_frame()
+            predict_pipeline = PredictPipeline()
+            results = predict_pipeline.predict(pred_df)
+            return render_template('home.html', result = results)
+        
+        except Exception as e:
+            import traceback
+            print("Error:", e)
+            traceback.print_exc()
+            return render_template('home.html', result="Error: " + str(e))
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
