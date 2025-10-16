7aebd0c956680f060d5e2b2c6938b5d53b6e49fe
Chhaya Varshney <chhaya3033@gmail.com>
2025-10-16 11:29:58 +0530

    Adding procfile and making changes in application.py

--- application.py
+++ application.py
@@ -33,5 +33,5 @@ def predict_datapoint():
         return render_template('home.html', result = results)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     application.run(host="0.0.0.0", port=port)
