951c2fa1c16074bdc696d2f821cc5637decb0298
Chhaya Varshney <chhaya3033@gmail.com>
2025-10-24 12:59:45 +0530

    Debug 17

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,7 @@
-from application import application  # import the Flask app object
+import os
+from application import application  # Import your Flask app
 
 if __name__ == "__main__":
-    application.run()
+    # Get port from environment (Elastic Beanstalk sets $PORT), fallback to 8080
+    port = int(os.environ.get("PORT", 8080))
+    application.run(host="0.0.0.0", port=port)
